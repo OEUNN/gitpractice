c643c61144552faacd77dca08fb79c0b7f357ca4
OEUNN <jyejyp5@gmail.com>
2022-11-15 09:12:50 +0900

    branch1-실행1 추가

--- src/gitpractice/Test.java
+++ src/gitpractice/Test.java
@@ -4,9 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("최초 실행문");
-		System.out.println("실행1");
-		System.out.println("실행2");
-		System.out.println("실행3");
+		System.out.println("branch1-살행1");
 	}
 
 }
